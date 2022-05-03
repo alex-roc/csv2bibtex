5a7e19e80537354e538fdeed2412373a175fd77c
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-05-02 22:35:32 -0400

    add: basic structure of bibtex

--- csv2bibtex/convert.py
+++ csv2bibtex/convert.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
+# bibtex structure
+
+bibtex = {
+    'type': '@article',
+    'citation_key': '',
+    'author': 'author = ',
+    'title': 'title = ',
+    'year': 'year = '
+}
+
 # load CSV
 data = pd.read_csv("csv2bibtex/data/urbana.csv")
 
