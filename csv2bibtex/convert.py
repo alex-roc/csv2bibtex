f126bc9eae8b932924540b6c3c542e37f124baa3
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-05-06 20:59:06 -0400

    add: transform to string

--- csv2bibtex/convert.py
+++ csv2bibtex/convert.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import numpy as np
 
 # bibtex structure
 
+bib_file = ''
 bibtex = {
     'type': '@article',
     'citation_key': '',
@@ -12,9 +14,19 @@ bibtex = {
 
 # load CSV
 data = pd.read_csv("csv2bibtex/data/urbana.csv")
+data_clean = data[['autor', 'titulo', 'anio']]
 
-# dataframe manipulation
+# dataframe manipulation to string
 
-# transform to strings with format
+for i in range(data_clean.shape[0]):
+    bib_file += str(bibtex['type']) + '{' + data_clean.iat[i, 0][0: 4] + str(data_clean.iat[i, 2]) + ',\n'
+    bib_file += '  ' + str(bibtex['author']) + '"'+  str(data_clean.iat[i, 0]) + '"' + ',\n'
+    bib_file += '  ' + str(bibtex['title']) + '"' + str(data_clean.iat[i, 1]) + '"' + ',\n'
+    bib_file += '  ' + str(bibtex['year']) + str(data_clean.iat[i, 2])
+    bib_file += '\n}\n'
+
 
 # save to bibtex
+file = open('csv2bibtex/output/convert.bib', 'w')
+file.write(bib_file)
+file.close()
