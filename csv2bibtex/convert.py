95d2443e9cde571419f66c96062c5a9f7ee1db40
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-06-14 08:39:41 -0400

    feat: tipos de items y publisher

--- csv2bibtex/convert.py
+++ csv2bibtex/convert.py
@@ -5,24 +5,30 @@ import numpy as np
 
 bib_file = ''
 bibtex = {
-    'type': '@article',
+    'type1': '@book',
+    'type2': '@article',
     'citation_key': '',
     'author': 'author = ',
     'title': 'title = ',
-    'year': 'year = '
+    'year': 'year = ',
+    'publisher': 'publisher = '
 }
 
 # load CSV
 data = pd.read_csv("csv2bibtex/data/urbana.csv")
-data_clean = data[['autor', 'titulo', 'anio']]
+data_clean = data[['autor', 'titulo', 'anio', 'editorial_institución', 'tipo_doc']]
 
 # dataframe manipulation to string
 
 for i in range(data_clean.shape[0]):
-    bib_file += str(bibtex['type']) + '{' + data_clean.iat[i, 0][0: 4] + str(data_clean.iat[i, 2]) + ',\n'
+    if data_clean.iat(i, 4) in ['libro', 'informe'] :
+        bib_file += str(bibtex['type1']) + '{' + data_clean.iat[i, 0][0: 4] + str(data_clean.iat[i, 2]) + ',\n'
+    else:
+        bib_file += str(bibtex['type1']) + '{' + data_clean.iat[i, 0][0: 4] + str(data_clean.iat[i, 2]) + ',\n'
     bib_file += '  ' + str(bibtex['author']) + '"'+  str(data_clean.iat[i, 0]) + '"' + ',\n'
     bib_file += '  ' + str(bibtex['title']) + '"' + str(data_clean.iat[i, 1]) + '"' + ',\n'
     bib_file += '  ' + str(bibtex['year']) + str(data_clean.iat[i, 2])
+    bib_file += '  ' + str(bibtex['publisher']) + str(data_clean.iat[i, 3])
     bib_file += '\n}\n'
 
 
