a1f5f635154fe4d69d28ea4da56b7d5cb66fa3b7
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-06-14 18:24:47 -0400

    fix: conditional with index

--- csv2bibtex/convert.py
+++ csv2bibtex/convert.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 # bibtex structure
 
@@ -16,19 +15,20 @@ bibtex = {
 
 # load CSV
 data = pd.read_csv("csv2bibtex/data/urbana.csv")
-data_clean = data[['autor', 'titulo', 'anio', 'editorial_institución', 'tipo_doc']]
+data_clean = data[['autor', 'titulo', 'anio', 'editorial_institucion', 'tipo_doc']]
 
 # dataframe manipulation to string
 
 for i in range(data_clean.shape[0]):
-    if data_clean.iat(i, 4) in ['libro', 'informe'] :
-        bib_file += str(bibtex['type1']) + '{' + data_clean.iat[i, 0][0: 4] + str(data_clean.iat[i, 2]) + ',\n'
+    if data_clean.iat[i, 4] in ['libro', 'informe', 'cartilla']:
+        bib_file += str(bibtex['type1']) 
     else:
-        bib_file += str(bibtex['type1']) + '{' + data_clean.iat[i, 0][0: 4] + str(data_clean.iat[i, 2]) + ',\n'
+        bib_file += str(bibtex['type2']) 
+    bib_file += '{' + data_clean.iat[i, 0].split()[0] + str(data_clean.iat[i, 2]) + ',\n'
     bib_file += '  ' + str(bibtex['author']) + '"'+  str(data_clean.iat[i, 0]) + '"' + ',\n'
     bib_file += '  ' + str(bibtex['title']) + '"' + str(data_clean.iat[i, 1]) + '"' + ',\n'
-    bib_file += '  ' + str(bibtex['year']) + str(data_clean.iat[i, 2])
-    bib_file += '  ' + str(bibtex['publisher']) + str(data_clean.iat[i, 3])
+    bib_file += '  ' + str(bibtex['year']) + str(data_clean.iat[i, 2]) + ',\n'
+    bib_file += '  ' + str(bibtex['publisher']) + '"' + str(data_clean.iat[i, 3]) + '"'
     bib_file += '\n}\n'
 
 
